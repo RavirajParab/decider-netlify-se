12f27c3b7689822d397c55040d62cfdd42723c48
Raviraj Parab <ravirajparab44@gmail.com>
2020-10-25 15:58:05 +0530

    cors header added

--- src/lambda/topcompanies.js
+++ src/lambda/topcompanies.js
@@ -3,6 +3,10 @@ exports.handler = async (event, context) => {
     const topCompanies = await getRSIForAllTopCompanies();
   return {
     statusCode: 200,
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json'
+      },
     body: JSON.stringify(topCompanies)
   };
 };
