8bd179be3d614575109dd2e5ff48af70692e47b1
Raviraj Parab <ravirajparab44@gmail.com>
2020-10-25 16:01:02 +0530

    header modified

--- src/lambda/topcompanies.js
+++ src/lambda/topcompanies.js
@@ -1,12 +1,14 @@
 const {getRSIForAllTopCompanies} = require("../utilities");
 exports.handler = async (event, context) => {
     const topCompanies = await getRSIForAllTopCompanies();
+    const headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+      };
   return {
     statusCode: 200,
-    headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json'
-      },
+    headers,
     body: JSON.stringify(topCompanies)
   };
 };
