792c2fddae4d08f0c64549bab7039e67e193c29c
Raviraj Parab <ravirajparab44@gmail.com>
2021-01-16 19:09:19 +0530

    drsilite added

---
+++ src/lambda/drsilite.js
@@ -0,0 +1,14 @@
+const {getDRSILite} = require("../utilities");
+exports.handler = async (event, context) => {
+    const quotes = await getDRSILite(event);
+    const headers = {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+    };
+  return {
+    statusCode: 200,
+    headers,
+    body: JSON.stringify(quotes)
+  };
+};
